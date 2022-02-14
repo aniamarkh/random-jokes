184817accc9dcf398314017b00d074cb50aff760
aniamarkh <aniamarkh@gmail.com>
2022-02-14 18:31:01 +0300

    feat: add joke function

---
+++ index.js
@@ -0,0 +1,24 @@
+const jokeText = document.querySelector('.joke');
+const jokeForm = document.querySelector('.jokeForm');
+const fireImg = document.querySelector('.fire');
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+
+async function getData() {
+    const url = `http://api.icndb.com/jokes/random`;
+    const res = await fetch(url);
+    const data = await res.json();
+    jokeText.innerHTML = data.value.joke;
+}
+getData();
+
+jokeForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const firstName = firstNameInput.value || 'Chuck';
+    const lastName = firstNameInput.value || 'Norris';
+    const urlname = `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`;
+    const res = await fetch(urlname);
+    const data = await res.json();
+    jokeText.innerHTML = data.value.joke;
+    fireImg.classList.add('on');
+})
